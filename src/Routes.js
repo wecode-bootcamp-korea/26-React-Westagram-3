ba172607919fec572fabb5bd9aabf1202e863d10
Elizabeth Jang <elizabethjang@Elizabethui-MacBookPro.local>
2021-10-20 17:06:07 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Routes.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+// 지원's 컴포넌트
+import LoginKang from './pages/jwKang/Login/Login';
+import MainKang from './pages/jwKang/Main/Main';
+
+// 연수's 컴포넌트
+import LoginLim from './pages/ysLim/Login/Login';
+import MainLim from './pages/ysLim/Main/Main';
+
+// 연정's 컴포넌트
+import LoginJang from './pages/yeonjeong/Login/Login';
+import MainJang from './pages/yeonjeong/Main/Main';
+
+
+export class Routes extends Component {
+    render(){
+        return(
+            <Router>
+                <Switch>
+                    <Route exact path='/login-jw' component={LoginKang} />
+                    <Route exact path='/main-jw' component={MainKang} />
+                    <Route exact path='/login-ys' component={LoginLim} />
+                    <Route exact path='/main-ys' component={MainLim} />
+                    <Route exact path='/login-yj' component={LoginJang} />
+                    <Route exact path='/main-yj' component={MainJang} />
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+
+export default Routes;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Nav extends React.Component {
+    render(){
+        return (
+            <nav>
+                <div className="navLogo">
+                    <img alt="Westagram logo image" src="/images/insta_logo.png"/>
+                    <h1>Westagram</h1>
+                </div>
+                <form className="navSearchForm">
+                    <input type="text" placeholder="검색"/>
+                    <i className="fas fa-search"></i>
+                </form>
+                <div className="navIcon">
+                    <img alt="explore icon" src="/images/explore.png"/>
+                    <img alt="heart icon" src="/images/heart.png"/>
+                    <img alt="profile icon" src="/images/profile.png"/>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Nav;
