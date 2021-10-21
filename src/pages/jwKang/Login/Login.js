6933dbf749a3d4caa354331c21617aebbb82dca4
betterplaywon <kkgi1234@gmail.com>
2021-10-21 15:43:37 +0900

    Add: 로그인 페이지/ 메인 페이지 세팅 완료

--- src/pages/jwKang/Login/Login.js
+++ src/pages/jwKang/Login/Login.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
+import './Login.scss';
 
 class LoginKang extends Component {
+  goMain = () => {
+    this.props.history.push('/Main-jw');
+  };
+
   render() {
-    return <div />;
+    return (
+      <div className="body">
+        <main className="login-container">
+          <h1 className="title">Westagram</h1>
+          <form className="userForm" id="userForm">
+            <input
+              required
+              className="idBox"
+              id="idBox"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+            />
+            <input
+              required
+              className="pwBox"
+              id="pwBox"
+              type="password"
+              placeholder="비밀번호"
+            />
+            <button className="loginBtn" id="loginBtn" onClick={this.goMain}>
+              로그인
+            </button>
+          </form>
+
+          <a href="_blank" className="forgot-pw">
+            비밀번호를 잊으셨나요?
+          </a>
+        </main>
+      </div>
+    );
   }
 }
 
