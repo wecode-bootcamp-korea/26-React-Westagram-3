a9c456ae01a33e08de0c7a73d50bcc86b2947dec
betterplaywon <kkgi1234@gmail.com>
2021-10-24 17:45:36 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현(그런데 이제 비번이 2개만 되도 켜지는..)

--- src/pages/jwKang/Login/Login.js
+++ src/pages/jwKang/Login/Login.js
@@ -1,8 +1,70 @@
 import React, { Component } from 'react';
+import './Login.scss';
 
 class LoginKang extends Component {
+  constructor() {
+    super();
+    this.state = {
+      idInputVal: '',
+      pwInputVal: '',
+      color: '#99d6ff',
+    };
+  }
+
+  handleIdInput = event => {
+    console.log(event.target.value);
+    this.setState({
+      idInputVal: event.target.value,
+    });
+  };
+
+  handlePwInput = event => {
+    console.log(event.target.value);
+    this.setState({
+      pwInputVal: event.target.value,
+    });
+  };
+
   render() {
-    return <div />;
+    return (
+      <div className="body">
+        <main className="login-container">
+          <h1 className="title">Westagram</h1>
+          <form className="userForm" id="userForm">
+            <input
+              required
+              className="idBox"
+              id="idBox"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={this.handleIdInput}
+            />
+            <input
+              required
+              className="pwBox"
+              id="pwBox"
+              type="password"
+              placeholder="비밀번호"
+              onChange={this.handlePwInput}
+            />
+            <button
+              className={
+                this.state.idInputVal.indexOf('@') !== -1 &&
+                this.state.pwInputVal > 5
+                  ? 'loginBtn2'
+                  : 'loginBtn'
+              }
+            >
+              로그인
+            </button>
+          </form>
+
+          <a href="_blank" className="forgot-pw">
+            비밀번호를 잊으셨나요?
+          </a>
+        </main>
+      </div>
+    );
   }
 }
 
